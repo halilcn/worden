4ff9f46e5c1adb7a2fe00faa7db5388e15a84b6e
Halil Can <54184509+halilcn@users.noreply.github.com>
2022-09-25 19:18:03 +0300

    add incoming request component

--- src/components/active-users/incoming-request-for-game/index.tsx
+++ src/components/active-users/incoming-request-for-game/index.tsx
@@ -1,9 +1,19 @@
+import WaitBird from "../../shared/wait-bird";
 import './index.scss'
 
 const IncomingRequestForGame = () => {
   return (
     <div className="incoming-request-game">
-      <div className="incoming-request-game__text">A user sent you a game request</div>
+      <div className="incoming-request-game__text">A user sent you a game request !</div>
+      <WaitBird/>
+      <div className="incoming-request-game__actions">
+        <div className="incoming-request-game__action-button incoming-request-game__action-button--reject">
+          reject
+        </div>
+        <div className="incoming-request-game__action-button incoming-request-game__action-button--accept">
+          accept
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/shared/wait-bird/index.tsx
+++ src/components/shared/wait-bird/index.tsx
@@ -1,7 +1,9 @@
-import 'index.scss'
+import { Player } from '@lottiefiles/react-lottie-player'
+
+import './index.scss'
 
 const WaitBird = () => {
-  return <div>test</div>
+  return <Player src="https://assets6.lottiefiles.com/private_files/lf30_rklapo5f.json" className="wait-bird" loop autoplay />
 }
 
 export default WaitBird
