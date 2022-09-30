42aa1d46c624b6e1a34767acf180d413ae86d09a
Halil Can <54184509+halilcn@users.noreply.github.com>
2022-09-30 23:59:24 +0300

    update login room logic

--- src/views/active-users/index.tsx
+++ src/views/active-users/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useMemo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 import ActiveUsersList from '../../components/active-users/active-users-list'
 import IncomingRequestForGame from '../../components/active-users/incoming-request-for-game'
 import RequestedForGame from '../../components/active-users/requested-for-game'
+import { ROUTER_PATHS } from '../../constants'
 import { RootState } from '../../store'
 import { gameRoomActions } from '../../store/reducers/game-room'
 import serverEvents from '../../utils/server-events'
@@ -11,6 +13,7 @@ import serverListeners from '../../utils/server-listeners'
 import './index.scss'
 
 const ActiveUsers = () => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const gameRoom = useSelector((state: RootState) => state.gameRoom)
 
@@ -21,6 +24,8 @@ const ActiveUsers = () => {
 
     serverListeners.gameAccepted(roomId => {
       serverEvents.loginGameRoom(roomId)
+      dispatch(gameRoomActions.setRoomId(roomId))
+      navigate(ROUTER_PATHS.game)
     })
   }, [])
 
--- src/views/game-dashboard/index.tsx
+++ src/views/game-dashboard/index.tsx
@@ -1,13 +1,25 @@
-import Room from '../../components/game-dashboard/room';
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-import './index.scss';
+import Room from '../../components/game-dashboard/room'
+import { ROUTER_PATHS } from '../../constants'
+import { RootState } from '../../store'
+import './index.scss'
 
 const GameDashboard = () => {
+  const navigate = useNavigate()
+  const gameRoom = useSelector((state: RootState) => state.gameRoom)
+
+  useEffect(() => {
+    if (!gameRoom.roomId) navigate(ROUTER_PATHS.activeUsers)
+  }, [])
+
   return (
     <div className="game-dashboard">
       <Room />
     </div>
-  );
-};
+  )
+}
 
-export default GameDashboard;
+export default GameDashboard
