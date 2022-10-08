3f249dcf46bc97063e23c014ef34c55a39d6e6c2
Halil Can <54184509+halilcn@users.noreply.github.com>
2022-10-08 17:56:54 +0300

    update calculate hook

--- src/hooks/useCalculateGamePoint.ts
+++ src/hooks/useCalculateGamePoint.ts
@@ -1,42 +1,34 @@
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import { GAME_WORDS_LENGTH, MIN_REMAINING_TIME } from '../constants'
-import store, { RootState } from '../store'
-import { gameActions } from '../store/reducers/game'
+import { RootState } from '../store'
 import serverEvents from '../utils/server-events'
 
-//TODO: ? -1 default ?
 const useCalculateGamePoint = (remainingTime: number = -1) => {
-  const dispatch = useDispatch()
-
   const game = useSelector((state: RootState) => state.game)
   const gameRoom = useSelector((state: RootState) => state.gameRoom)
   const auth = useSelector((state: RootState) => state.auth)
 
   const processCalculatePoint = () => {
-    //TODO: review
-    const totalPoint =
-      game.wordUserAnswers.length === 0
-        ? 0
-        : game.wordUserAnswers
-            .map((word, wordIndex) => {
-              const wordUserSplitted = word.split('')
-              const wordAnswer = game.wordAnswers[wordIndex].split('')
+    let totalPoint = 0
 
-              return wordAnswer.filter((wordAnswerCharacter, wordAnswerKey) => wordAnswerCharacter === wordUserSplitted[wordAnswerKey])
-                .length
-            })
-            .reduce((a, b) => a + b)
+    if (game.wordUserAnswers.length > 0) {
+      totalPoint = game.wordUserAnswers
+        .map((word, wordIndex) => {
+          const wordUserSplitted = word.split('')
+          const wordAnswer = game.wordAnswers[wordIndex].split('')
 
-    console.log(totalPoint)
+          return wordAnswer.filter((wordAnswerCharacter, wordAnswerKey) => wordAnswerCharacter === wordUserSplitted[wordAnswerKey]).length
+        })
+        .reduce((a, b) => a + b)
+    }
 
     serverEvents.sendPointOfUser({ roomId: gameRoom.roomId as string, userSocketId: auth.socketId, point: totalPoint })
   }
 
   useEffect(() => {
-    if (game.currentWordIndex === GAME_WORDS_LENGTH - 1) processCalculatePoint()
-    if (remainingTime === MIN_REMAINING_TIME) processCalculatePoint()
+    if (game.currentWordIndex === GAME_WORDS_LENGTH - 1 || remainingTime === MIN_REMAINING_TIME) processCalculatePoint()
   }, [game.currentWordIndex, remainingTime])
 }
 
