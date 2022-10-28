5f72762ffe773a0221090ae9b8df4cfeb9e8ea93
halilcn <iletisimhalilcan@gmail.com>
2022-10-28 15:44:36 +0300

    clean todo list

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import { gameRoomActions } from './store/reducers/game-room'
 import './styles/index.scss'
 import serverListeners from './utils/server-listeners'
 
-//TODO: puanlamada 2 kere socket.on dinleniyor gibi ?
-
 const App = () => {
   const router = createBrowserRouter(routes)
   const dispatch = useDispatch()
