193718a0964080f219f056fb80b522f1d321c00b
halilcn <iletisimhalilcan@gmail.com>
2022-11-20 13:37:53 +0300

    add new component for seperate logics welcome page

---
+++ src/components/welcome-user/welcome/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { ROUTER_PATHS, USERNAME_LOCALSTORAGE } from '../../../constants'
+import { RootState } from '../../../store'
+import { authActions } from '../../../store/reducers/auth'
+import serverEvents from '../../../utils/server-events'
+import serverListeners from '../../../utils/server-listeners'
+
+const Welcome = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const authState = useSelector((state: RootState) => state.auth)
+
+  const [alreadyExist, setAlreadyExist] = useState<boolean>(false)
+
+  const usernameRef = useRef<string>('')
+
+  useEffect(() => {
+    if (authState.username) navigate(ROUTER_PATHS.activeUsers)
+
+    serverListeners.alreadyExistUsername(() => {
+      setAlreadyExist(true)
+    })
+
+    serverListeners.correctUsernameToLogin((socketId: string) => {
+      localStorage.setItem(USERNAME_LOCALSTORAGE, usernameRef.current)
+      dispatch(authActions.setUsername(usernameRef.current))
+      dispatch(authActions.setSocketId(socketId))
+      navigate(ROUTER_PATHS.activeUsers)
+    })
+  }, [])
+
+  const handleSaveUsername = () => {
+    if (usernameRef.current === '') return
+    serverEvents.login(usernameRef.current)
+  }
+
+  const handleChangeUsername = (username: string) => {
+    usernameRef.current = username
+  }
+
+  return (
+    <div className="welcome">
+      <div className="welcome__text">Welcome !</div>
+      <input
+        onKeyPress={e => e.key === 'Enter' && handleSaveUsername()}
+        onChange={e => handleChangeUsername(e.target.value)}
+        placeholder="Type a username to play..."
+        className="welcome__username-input"
+      />
+      {alreadyExist && <div className="welcome__error">Already exist the username</div>}
+      <div onClick={handleSaveUsername} className="welcome__save-username">
+        Login
+      </div>
+    </div>
+  )
+}
+
+export default Welcome
