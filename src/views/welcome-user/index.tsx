159a21f0fbefade0391c118e6b5a27042568cf45
halilcn <iletisimhalilcan@gmail.com>
2022-11-20 13:52:28 +0300

    add timeout server component

--- src/views/welcome-user/index.tsx
+++ src/views/welcome-user/index.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from 'react'
 import { useSelector } from 'react-redux'
 
+import TimeoutServer from '../../components/welcome-user/timeout-server'
 import WaitingServer from '../../components/welcome-user/waiting-server'
 import Welcome from '../../components/welcome-user/welcome'
 import { RootState } from '../../store'
@@ -15,11 +16,12 @@ enum ActiveContent {
 const WelcomeUser = () => {
   const socketServerState = useSelector((state: RootState) => state.socketServer)
 
-  const [activeContent, setActiveContent] = useState<ActiveContent>(ActiveContent.waitingServer)
+  const [activeContent, setActiveContent] = useState<ActiveContent>(ActiveContent.timeoutServer)
 
   const memorizedActiveContent = useMemo(() => {
     if (activeContent === ActiveContent.welcome) return <Welcome />
     if (activeContent === ActiveContent.waitingServer) return <WaitingServer />
+    if (activeContent === ActiveContent.timeoutServer) return <TimeoutServer />
   }, [activeContent])
 
   return <div className="welcome">{memorizedActiveContent}</div>
